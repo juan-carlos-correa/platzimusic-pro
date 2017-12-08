e6ab6b7bb86d8a5b6ade273c03e3471dbdb4c71f
Juan Carlos Correa Herrera <juan.correa@varangard.com>
2017-12-08 10:48:21 -0600

    Ejemplo de computed y watchers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    label(for='yearBirthday') Año de nacimiento
+    input(v-model="yearBirthday" id='yearBirthday')
+    p(v-if="yearBirthday") Tú edad: {{ age }}
+    p Valor anterior: {{ oldVal }}
+    p Valor nuevo: {{ newVal }}
 </template>
 
 <script>
@@ -9,7 +12,31 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      yearBirthday: 0,
+      oldVal: 0,
+      newVal: 0
+    }
+  },
+
+  /**
+   * funciones que generan nuevas propiedades del componente en base a otras propiedades.
+   * Deben ser funciones, se pueden usar como expresiones en la UI
+   */
+  computed: {
+    age () {
+      return (new Date().getFullYear()) - this.yearBirthday
+    }
+  },
+
+  /**
+   * Semejante al patrón Observer
+   * No devuelven un valor, no son propiedades y tampoco pueden ser usadas en propiedades
+   */
+  watch: {
+    age (newVal, oldVal) {
+      console.log(newVal, oldVal)
+      this.oldVal = oldVal
+      this.newVal = newVal
     }
   }
 }
