2824abfa888160ba158da2201f9f4afd4f4fbfc1
Juan Carlos Correa Herrera <juan.correa@varangard.com>
2017-12-11 16:30:02 -0600

    Add Vuex

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.container
+.container(v-if="track && track.id")
   .columns
     .column.is-3.has-text-centered
       figure.media-left
@@ -8,11 +8,12 @@
         p
           a.button.is-primary.is-large
             span.icon(@click="selectTrack")
+              i.fa.fa-play
 
     .column.is-8
       .panel
         .panel-heading
-          h1.title {{ track.name }}
+          h1.title {{ trackTitle }}
         .panel-block
           article.media
             .media-content
@@ -28,25 +29,29 @@
 </template>
 
 <script>
-import trackService from '@/services/track'
+import { mapState, mapActions, mapGetters } from 'vuex'
+
 import trackMixin from '@/mixins/track'
 
 export default {
-  mixins: [ trackMixin ],
+  mixins: [trackMixin],
 
-  data () {
-    return {
-      track: {}
-    }
+  computed: {
+    ...mapState(['track']),
+    ...mapGetters(['trackTitle'])
   },
 
   created () {
     const id = this.$route.params.id
 
-    trackService.getById(id)
-      .then(res => {
-        this.track = res
-      })
+    if (!this.track || !this.track.id || this.track.id !== id) {
+      this.getTrackById({ id })
+        .then(() => console.log('Track loaded...'))
+    }
+  },
+
+  methods: {
+    ...mapActions(['getTrackById'])
   }
 }
 </script>
@@ -54,6 +59,10 @@ export default {
 <style lang="scss" scoped>
 .columns {
   margin: 20px;
+}
+
+.button-bar {
+  margin-top: 20px;
 }
 </style>
 
